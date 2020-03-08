032d99cd8fdc6122a1e00d7b7ede6d359653aea2
PedroJG <pedrojg2001@gmail.com>
2020-03-08 22:03:34 +0100

    Terminado?

--- JAVASCRIPT/functions.js
+++ JAVASCRIPT/functions.js
@@ -28,4 +28,34 @@ $(document).ready(function() {
 
     prevSlide.addClass("active");
   });
+
+  $("#right-arrow2").click(function() {
+    var currentSlide = $(".slide2.active");
+    var nextSlide = currentSlide.next();
+
+    currentSlide.removeClass("active");
+
+    if (nextSlide.length == 0) {
+      $(".slide2")
+        .first()
+        .addClass("active");
+    }
+
+    nextSlide.addClass("active");
+  });
+
+  $("#left-arrow2").click(function() {
+    var currentSlide = $(".slide2.active");
+    var prevSlide = currentSlide.prev();
+
+    currentSlide.removeClass("active");
+
+    if (prevSlide.length == 0) {
+      $(".slide2")
+        .last()
+        .addClass("active");
+    }
+
+    prevSlide.addClass("active");
+  });
 });
